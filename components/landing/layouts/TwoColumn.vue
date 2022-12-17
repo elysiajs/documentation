420f9c455a8262cd29ac781f5fe248d34755e618
saltyaom <saltyaom@gmail.com>
2022-12-18 00:40:46 +0700

    :tada: feat: landing

--- components/landing/layouts/TwoColumn.vue
+++ components/landing/layouts/TwoColumn.vue
@@ -1,5 +1,5 @@
 <template>
-    <article class="flex flex-col-reverse md:grid-col grid-cols-1 md:grid-cols-2 items-center justify-center gap-10 md:gap-16 py-12">
+    <article class="flex flex-col-reverse md:grid md:grid-col grid-cols-1 md:grid-cols-2 items-center justify-center gap-10 md:gap-16 py-12">
         <section class="flex flex-col justify-start flex-1">
             <slot name="left" />
         </section>
