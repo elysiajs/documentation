7b5ec65cb923bdc8d0f86836afe40b5ceb399a48
saltyaom <saltyaom@gmail.com>
2023-09-05 17:38:20 +0700

    :blue_book: doc: rename new-app to app

--- components/midori/hero.vue
+++ components/midori/hero.vue
@@ -165,7 +165,7 @@
             <code
                 class="text-blue-500 font-mono font-medium text-lg bg-blue-200/25 dark:bg-blue-500/20 px-6 py-2.5 rounded-full"
             >
-                bun create elysia new-app
+                bun create elysia app
             </code>
         </section>
         <p class="flex justify-center items-center gap-2 text-gray-400">
