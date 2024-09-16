f7ea7f39c0623cd4817d7b369eef5c9f131718b7
saltyaom <saltyaom@gmail.com>
2024-09-16 21:46:26 +0700

    :blue_book: doc(landing): resize benchmark size on mobile

--- components/fern/benchmark.vue
+++ components/fern/benchmark.vue
@@ -3,7 +3,7 @@
         <div class="grid">
             <div class="fog" />
         </div>
-        <header class="flex flex-col sm:flex-row md:flex-col justify-around md:justify-center z-10 w-full md:max-w-[10.5rem]">
+        <header class="flex flex-row md:flex-col justify-around md:justify-center z-10 w-full md:max-w-[10.5rem]">
             <div class="title md:mb-10">
                 <h3 class="text-gradient from-pink-400 to-fuchsia-400">21x</h3>
                 <p>faster than Express</p>
@@ -105,11 +105,11 @@
     @apply flex flex-col justify-start items-start gap-0.5 text-gray-400;
 
     & > h3 {
-        @apply text-8xl font-bold;
+        @apply text-7xl sm:text-8xl font-bold;
     }
 
     & > p {
-        @apply text-xl;
+        @apply text-lg sm:text-xl;
     }
 }
 
