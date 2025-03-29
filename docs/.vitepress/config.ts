4dc14908e66a530e62362b791fc5941903280cec
Okinea Dev <hi@okinea.dev>
2025-03-29 10:33:49 +0100

    update plugin configuration

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -46,14 +46,10 @@ export default defineConfig({
 		plugins: [
 			tailwindcss() as any,
 			llmstxt({
-				customTemplateVariables: {
-					description: 'Ergonomic Framework for Humans',
-					details: 'Elysia is an ergonomic framework for Humans. With end-to-end type safety and great developer experience. Elysia is familiar, fast, and first class TypeScript support with well-thought integration between services whether it\'s tRPC, Swagger or WebSocket.'
-				},
-				ignoreFiles: [
-					"index.md",
-					"table-of-content.md"
-				]
+				description: 'Ergonomic Framework for Humans',
+				details:
+					"Elysia is an ergonomic framework for Humans. With end-to-end type safety and great developer experience. Elysia is familiar, fast, and first class TypeScript support with well-thought integration between services whether it's tRPC, Swagger or WebSocket.",
+				ignoreFiles: ['index.md', 'table-of-content.md']
 			}),
 			// GitChangelog({
 			//     // Fill in your repository URL here
