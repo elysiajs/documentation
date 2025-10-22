c3d7dbec1423423d804ddf186aa62b9a6e21a2f8
saltyaom <saltyaom@gmail.com>
2025-10-22 12:36:01 +0700

    :tada: feat: add proof of work for ai

--- docs/components/arona/arona.vue
+++ docs/components/arona/arona.vue
@@ -392,7 +392,7 @@ const init = ref(false)
 
 let controller: AbortController | undefined
 
-const url = 'http://localhost:3000'
+const url = 'https://arona.elysiajs.com'
 
 watch(
     () => model.value,
