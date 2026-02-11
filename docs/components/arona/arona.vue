1f63bcd8562aa9616ed821860baaa25aee568e77
saltyaom <saltyaom@gmail.com>
2026-02-12 02:51:58 +0700

    :wrench: fix: only navigate in page when url is link to documentation on arona

--- docs/components/arona/arona.vue
+++ docs/components/arona/arona.vue
@@ -1034,11 +1034,13 @@ function reRouteLink(link: HTMLAnchorElement) {
     const src = link.href.slice(link.href.indexOf('/', 11))
 
     link.addEventListener('click', (e) => {
-        e.preventDefault()
+	    if(src.startsWith('https://elysiajs.com')) {
+	        e.preventDefault()
 
-        router.go(src)
-        _isExpanded.value = false
-        if (size.width.value < 640) model.value = false
+	        router.go(src)
+	        _isExpanded.value = false
+	        if (size.width.value < 640) model.value = false
+	    }
     })
 }
 
